5614c44b48371ad25d41c3db6ebb697c94e6678b
HikariYlan <ylan.nicolas52600@gmail.com>
2026-02-22 03:08:02 +0100

    feat: Continent enum

---
+++ src/Enum/ContinentEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum ContinentEnum: string
+{
+    case Africa = 'africa';
+    case America = 'americas';
+    case Asia = 'asia';
+    case Europe = 'europe';
+    case Oceania = 'oceania';
+}
