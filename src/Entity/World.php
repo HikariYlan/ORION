72f7b13bdc8551116d75f8a961751cf6fa185b7e
HikariYlan <ylan.nicolas52600@gmail.com>
2026-02-26 22:16:58 +0100

    feat: world entity created

---
+++ src/Entity/World.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\TypesEnum;
+use App\Repository\WorldRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorldRepository::class)]
+class World
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'worlds')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    #[ORM\Column]
+    private ?\DateTime $creationDate = null;
+
+    #[ORM\Column(type: Types::SIMPLE_ARRAY, enumType: TypesEnum::class)]
+    private array $type = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTime
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTime $creationDate): static
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * @return TypesEnum[]
+     */
+    public function getType(): array
+    {
+        return $this->type;
+    }
+
+    public function setType(array $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
