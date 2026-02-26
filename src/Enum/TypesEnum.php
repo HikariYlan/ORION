568f021a3018c240c960a05ce02f04e0021d15fc
HikariYlan <ylan.nicolas52600@gmail.com>
2026-02-26 22:17:28 +0100

    feat: Types enum created

---
+++ src/Enum/TypesEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enum;
+
+enum TypesEnum: string
+{
+    case SciFi = 'Sci-Fi';
+    case Fantasy = 'Fantasy';
+    case Mystery = 'Mystery';
+    case Romance = 'Romance';
+    case Horror = 'Horror';
+    case Thriller = 'Thriller';
+    case Polar = 'Polar';
+    case Comedy = 'Comedy';
+    case Historical = 'Historical';
+}
